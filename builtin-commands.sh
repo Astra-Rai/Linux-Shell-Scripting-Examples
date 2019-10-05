c0b7f2fe2ca6450c9a71770eda3dc5bdeadcc23a
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-10-05 17:15:18 -0400

    Section 2 of 5 | Commands: pwd, cd, echo, cd ..

---
+++ builtin-commands.sh
@@ -0,0 +1,38 @@
+
+#!/bin/bash
+
+
+#Exercises and notes from Section 2 of 5
+
+# note: #! = Shebang
+
+#pwd, presemt working directory
+
+pwd
+
+#cd command to change directory
+
+cd
+
+#print what is within quotes to screen
+
+cd ..
+
+#check if a command is builtin
+#for example, check if echo is a built in command
+
+echo "hello world"
+
+#move to parent or directory above current directory
+
+echo -a 
+
+#set variable SKILL to the string "shell scripting"
+
+SKILL="shell scripting"
+
+#use the variable SKILL within a string and print to screen
+
+echo "I want to be at ${SKILL}.  That's why I practice ${SKILL}. "
+
+
