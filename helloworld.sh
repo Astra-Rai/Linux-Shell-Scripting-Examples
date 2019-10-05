29642845812e802c13d2a559a63a45a14a94efd4
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-10-05 17:13:46 -0400

    Section 1 of 5 | Variables and the Echo Command

--- helloworld.sh
+++ helloworld.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-
+#store the string 'Astra Rai Daniels in the variable NAME'
 NAME="Astra Rai Daniels"
 
+#print the string 'Astra Rai Daniels is in the house using the echo command'
 echo "${NAME} is in the house." 
