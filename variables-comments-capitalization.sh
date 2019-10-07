a3854ce559603b2ec47adf15b7a5d3c3dad243de
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-10-07 00:56:21 -0400

    Section 3 of 5 | Vars, Comments, Capitalization

---
+++ variables-comments-capitalization.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#variables can be in all caps, but must referenced as such
+#no white space on sides of equals sign 
+#it is convention, not a rule that all variables are in caps
+
+NAME="Astra"
+
+#pring variable coby enclosed by ${variable name here}, with $ leading the way
+
+#short exercise
+
+SKILL="shell scripting"
+
+echo "I want to be good at ${SKILL}"
+
+#variables can be reassigned
+#if you reassign a variable note in a comment directly above the reassignment of that variable
+#use a blank line to seperate two diffrent blocks of code
+
+
+
+
